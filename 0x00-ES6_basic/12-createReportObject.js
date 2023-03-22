396813b9af774b8915b8b802dcc533c8e0ee1fad
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-03-22 10:20:02 +0000

    reate report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
-  getNumberOfDepartments(employees) return Object.keys(employees).length;	
   return {
     allEmployees: {
-       ...employeesList,
+      ...employeesList,
     },
+    getNumberOfDepartments(employees) { return Object.keys(employees).length; },
   };
 }
